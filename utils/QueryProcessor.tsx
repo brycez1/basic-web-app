781a5fcdfbc486d5f587ff7c01954e52f668dc6b
brycez <brycez@andrew.cmu.edu>
2024-03-14 15:45:03 -0400

    added name query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,5 +7,10 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("What is your name?")) {
+    return (
+      "Bryce"
+    );
+  }
   return "";
 }
